6b14f732fda41333dd93b5c2f9d631424d14b3fb
Xuehai Zhou <xuehaizhou9@gmail.com>
2021-04-04 11:17:49 +0800

    dungeonGame finished

---
+++ 174-DungeonGame/dungeon.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        const int m = dungeon.size();
+        const int n = dungeon[0].size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, INT_MAX));
+        dp[m][n-1] = dp[m-1][n] = 1;
+        
+        for (int i = m; i > 0; --i) {
+            for (int j = n; j > 0; --j) {
+                dp[i-1][j-1] = max(1, min(dp[i-1][j], dp[i][j-1]) - dungeon[i-1][j-1]);
+            }
+        }
+        return dp[0][0];
+    }
+};
